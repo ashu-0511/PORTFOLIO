a2e1f35db3fca3d9e55ae4baa4d9b17ef2261d52
ashu-0511 <narulaashutosh@icloud.com>
2024-03-10 19:45:04 +0530

    files added

---
+++ src/Main.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { ReactTyped } from "react-typed";
+import './Main.css'
+
+const Main = () => {
+    return (
+        <div>
+            <section className="firstsection">
+                <div className="leftsection">Hi,My name is <span className="purple">Ashutosh</span>
+                    <div>and I'm a passionate</div>
+                    <div id='element'><ReactTyped strings={["Front-end Developer", "Web Developer"]} typeSpeed={40} /></div>
+
+                </div>
+                <div className="rightsection">IMAGE</div>
+            </section>
+        </div>
+    )
+}
+
+export default Main;
